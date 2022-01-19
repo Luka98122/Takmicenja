c3ce3b9736783249c3918ffffb2781506846b991
Luka98122 <luka98122@gmail.com>
2022-01-19 14:45:24 +0100

    Fixed Code
    Added and tested test cases

--- Sedmi Razred/Opstinsko 2010/PARE.py
+++ Sedmi Razred/Opstinsko 2010/PARE.py
@@ -1,3 +1,11 @@
+# Test cases
+# 2 3 10 10 15 = 6
+# 2 3 10 5 15  = -2
+#
+# My test cases
+# 10 1 5 2 5   = -10240
+# 10 1 5 1 2 =  -120
+
 stolovi = int(input())
 vremeZaZaradu = int(input())
 pare = 0
@@ -8,6 +16,9 @@ DugovinaSefu = 0
 for i in range(1, Dani + 1):
     if i % vremeZaZaradu == 0 and i != 0:
         pare = pare + stolovi
+    if pare < 0:
+        DugovinaSefu = DugovinaSefu - pare
+        pare = 0
     if DugovinaSefu > 0 and pare > 0:
         if pare >= DugovinaSefu:
             pare = pare - DugovinaSefu
@@ -15,12 +26,10 @@ for i in range(1, Dani + 1):
         else:
             DugovinaSefu = DugovinaSefu - pare
             pare = 0
-    if pare >= minimum:
-        pare = pare - minimum
-        stolovi = stolovi * 2
+    while pare >= minimum:
+        if pare >= minimum:
+            pare = pare - minimum
+            stolovi = stolovi * 2
     if i % vreme == 0:
         pare = pare - stolovi * 2
-    if pare < 0:
-        DugovinaSefu = DugovinaSefu - pare
-        pare = 0
 print(pare - DugovinaSefu)
